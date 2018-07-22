c0aff5b63cc6a1c1864f2bbf6cf7e7c090744b67
Patrick Mackow <patrickmackow@gmail.com>
2018-07-22 12:39:10 -0400

    Extract all data from html

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,18 +1,49 @@
 const cheerio = require("cheerio");
+const url = require("url");
+
+extractTitle = (el, $) => {
+  return $(".title a", el)
+    .text()
+    .trim();
+};
+
+extractAuthor = (el, $) => {
+  return $(".postBy a", el)
+    .eq(0)
+    .text()
+    .trim();
+};
+
+extractUrl = (el, $) => {
+  const relative = $(".title a", el).attr("href");
+  return url.resolve("https://www.pathofexile.com/forum", relative);
+};
+
+extractViews = (el, $) => {
+  return $(".post-stat span", el)
+    .text()
+    .trim();
+};
+
+extractPostDate = (el, $) => {
+  return $(".postBy .post_date", el)
+    .text()
+    .trim()
+    .slice(2);
+};
 
 parser = body => {
-  let rows;
   const $ = cheerio.load(body);
 
-  rows = $("#view_forum_table tbody tr")
+  const rows = $("#view_forum_table tbody tr")
     .map((i, el) => {
-      const row = {};
-      row.title = "";
-      row.author = "";
-      row.url = "";
-      row.views = "";
-      row.postDate = "";
-      return row;
+      return {
+        title: extractTitle(el, $),
+        author: extractAuthor(el, $),
+        url: extractUrl(el, $),
+        views: extractViews(el, $),
+        postDate: extractPostDate(el, $)
+      };
     })
     .get();
 
