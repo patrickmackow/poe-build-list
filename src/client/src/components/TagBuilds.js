50d716c302e0126c753c690c1cc3b66976f27d8f
Patrick Mackow <patrickmackow@gmail.com>
2018-10-04 15:59:14 -0400

    Add filters to TagBuilds

--- src/client/src/components/TagBuilds.js
+++ src/client/src/components/TagBuilds.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import BuildsTable from "./BuildsTable";
+import VersionFilter from "./VersionFilter";
 
 class TagBuilds extends Component {
   constructor(props) {
@@ -8,8 +9,13 @@ class TagBuilds extends Component {
 
     this.state = {
       loading: true,
-      builds: []
+      builds: [],
+      version: "3.4",
+      class: "all"
     };
+
+    this.handleVersionChange = this.handleVersionChange.bind(this);
+    this.handleClassChange = this.handleClassChange.bind(this);
   }
 
   componentDidMount() {
@@ -24,24 +30,84 @@ class TagBuilds extends Component {
       );
   }
 
+  handleVersionChange(e) {
+    this.setState({ version: e.target.value });
+  }
+
+  handleClassChange(e) {
+    this.setState({ class: e.target.value });
+  }
+
+  filterBuilds(builds) {
+    return builds.filter(build => {
+      if (build.gameClass === this.state.class || this.state.class === "all") {
+        if (build.version === this.state.version) {
+          return true;
+        }
+      }
+
+      return false;
+    });
+  }
+
   render() {
     const { loading, builds } = this.state;
     const { tag } = this.props.match.params;
 
-    const buildsViews = loading ? (
-      <p>Loading...</p>
-    ) : (
-      <BuildsTable builds={builds} />
-    );
+    let buildsView;
+    if (loading) {
+      buildsView = <p>Loading...</p>;
+    } else {
+      const filteredBuilds = this.filterBuilds(builds);
+      buildsView = (
+        <div>
+          <VersionFilter
+            value={this.state.version}
+            builds={builds}
+            onChange={this.handleVersionChange}
+          />
+          <ClassFilter
+            value={this.state.class}
+            onChange={this.handleClassChange}
+          />
+          <BuildsTable builds={filteredBuilds} />
+        </div>
+      );
+    }
 
     return (
       <div>
         <Link to="/">Back to Home</Link>
         <h1>{tag}</h1>
-        {buildsViews}
+        {buildsView}
       </div>
     );
   }
 }
+
+const ClassFilter = props => {
+  const classes = [
+    "all",
+    "duelist",
+    "marauder",
+    "ranger",
+    "scion",
+    "shadow",
+    "templar",
+    "witch"
+  ];
+
+  return (
+    <select value={props.value} onChange={props.onChange}>
+      {classes.map(c => {
+        return (
+          <option key={c} value={c} children={c}>
+            {c}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
 
 export default TagBuilds;
