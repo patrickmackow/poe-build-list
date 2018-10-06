73f04da5102636dcb8cc77420c9f40f9d4532e4c
Patrick Mackow <patrickmackow@gmail.com>
2018-10-05 23:35:44 -0400

    Add tag search bar

--- src/client/src/components/Home.js
+++ src/client/src/components/Home.js
@@ -2,10 +2,36 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tag: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ tag: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.history.push("/tag/" + this.state.tag);
+  }
+
   render() {
     return (
       <div>
         <h1>Path of Exile Build List</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search by tag"
+            value={this.state.tag}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <br />
         <div>
           <Link to="/class/duelist">Duelist</Link>
         </div>
