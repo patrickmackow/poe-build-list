b66b2384d9901bdf2575d556460b7a3879968624
Patrick Mackow <patrickmackow@gmail.com>
2018-10-14 11:15:22 -0400

    Display builds in divs instead of a table. Added tag badges

--- src/client/src/components/BuildRow.js
+++ src/client/src/components/BuildRow.js
@@ -5,18 +5,43 @@ class BuildRow extends Component {
     const { build } = this.props;
 
     return (
-      <tr>
-        <td>
-          <a rel="external" href={build.url}>
-            {build.title}
-          </a>
-        </td>
-        <td>{build.views}</td>
-        <td>{build.replies}</td>
-        <td>{build.latestPost}</td>
-      </tr>
+      <div className="row border-bottom">
+        <div className="row">
+          <div className="col-12" />
+        </div>
+        <div className="col-sm-10">
+          <div className="row">
+            <div className="col-12">
+              <a rel="external" href={build.url}>
+                {build.title}
+              </a>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              {build.generatedTags.map(tag => tagBadge(tag))}
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-2">
+          <div className="row">
+            <div className="col-12 text-sm-right">{build.views}</div>
+          </div>
+          <div className="row">
+            <div className="col-12 text-sm-right">{build.replies}</div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
+
+const tagBadge = tag => {
+  return (
+    <span className="badge badge-pill badge-primary mr-1 text-uppercase">
+      {tag}
+    </span>
+  );
+};
 
 export default BuildRow;
--- src/client/src/components/BuildsTable.js
+++ src/client/src/components/BuildsTable.js
@@ -9,22 +9,7 @@ class BuildsTable extends Component {
       <BuildRow key={build._id} build={build} />
     ));
 
-    return (
-      <div>
-        <table>
-          {/* TODO: Generate these from props */}
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Views</th>
-              <th>Replies</th>
-              <th>Latest Post</th>
-            </tr>
-          </thead>
-          <tbody>{buildRows}</tbody>
-        </table>
-      </div>
-    );
+    return <div>{buildRows}</div>;
   }
 }
 
