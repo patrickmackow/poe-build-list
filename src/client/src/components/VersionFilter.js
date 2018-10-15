cf0dffe9cfcd57479079ed551f713f6693c719d8
Patrick Mackow <patrickmackow@gmail.com>
2018-10-15 12:40:07 -0400

    Added minimum build version. Added styling for version

--- src/client/src/components/VersionFilter.js
+++ src/client/src/components/VersionFilter.js
@@ -1,26 +1,42 @@
 import React, { Component } from "react";
 
+const MINIMUM_VERSION = "3.0";
+
 class VersionFilter extends Component {
   constructor(props) {
     super(props);
 
     this.versionSet = new Set();
     this.props.builds.map(
-      build => (build.version ? this.versionSet.add(build.version) : null)
+      build =>
+        build.version && build.version >= MINIMUM_VERSION
+          ? this.versionSet.add(build.version)
+          : null
     );
   }
 
   render() {
+    const patches = Array.from(this.versionSet)
+      .sort((a, b) => b - a)
+      .map(version => (
+        <option key={version} value={version}>
+          {version}
+        </option>
+      ));
+
     return (
-      <select value={this.props.value} onChange={this.props.onChange}>
-        {Array.from(this.versionSet)
-          .sort((a, b) => b - a)
-          .map(version => (
-            <option key={version} value={version}>
-              {version}
-            </option>
-          ))}
-      </select>
+      <div className="form-inline mb-2">
+        <label className="col-form-label col-form-label-sm text-muted text-uppercase mr-2">
+          Patch
+        </label>
+        <select
+          className="col-auto form-control form-control-sm custom-select border-0 bg-light"
+          value={this.props.value}
+          onChange={this.props.onChange}
+        >
+          {patches}
+        </select>
+      </div>
     );
   }
 }
