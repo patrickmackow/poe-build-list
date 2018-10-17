432a5fc445032882911221188cbcb43dd7277ffc
Patrick Mackow <patrickmackow@gmail.com>
2018-10-16 23:02:47 -0400

    Refactored inputs and state into AutoSuggest for easier state management

--- src/client/src/components/AutoSuggest.js
+++ src/client/src/components/AutoSuggest.js
@@ -4,35 +4,112 @@ class AutoSuggest extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { visible: false, index: undefined, suggestion: "" };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.setVisibility = this.setVisibility.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.activeIndex = this.isActive.bind(this);
   }
 
-  isActive() {
-    return false;
+  handleChange(e) {
+    if (!this.state.visible) this.setVisibility(true);
+    this.props.onChange(e);
+  }
 
-    /*if (index !== undefined) {
-      index = index < 0 ? (index = arr.length + index) : index;
-      classString += index % arr.length === i ? " active" : "";
-    }*/
+  setVisibility(v) {
+    this.setState({ visible: v });
+    if (!v) {
+      this.setState({ index: undefined });
+    }
+  }
+
+  handleKeyDown(e) {
+    if (e.key === "Escape") {
+      this.setVisibility(false);
+    } else if (e.keyCode === 13) {
+      //e.preventDefault();
+    } else if (e.keyCode === 38) {
+      // Arrow up
+      e.preventDefault();
+
+      if (!this.state.visible) return;
+
+      if (this.state.index !== undefined) {
+        this.setState({ index: this.state.index - 1 });
+      } else {
+        this.setState({ index: 0 });
+      }
+    } else if (e.keyCode === 40) {
+      // Arrow down
+      e.preventDefault();
+
+      if (!this.state.visible) return;
+
+      if (this.state.index !== undefined) {
+        this.setState({ index: this.state.index + 1 });
+      } else {
+        this.setState({ index: 0 });
+      }
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.onChange(e);
+    this.setVisibility(false);
+  }
+
+  isActive(index, i, arr) {
+    if (index === undefined) {
+      return false;
+    }
+
+    index = index < 0 ? (index = arr.length + index) : index;
+    return index % arr.length === i ? true : false;
   }
 
   render() {
-    const data = this.props.data.filter(d => d.includes(this.props.tag));
-    let { index } = this.props;
+    const data = this.props.data.filter(d => d.includes(this.props.value));
+    let { index } = this.state;
 
-    const suggestions = data.map(d => {
-      const active = this.isActive();
+    // TODO: add a suggestion when filtered data is empty
+    const suggestions = data.map((d, i, data) => {
+      const active = this.isActive(index, i, data);
 
       return (
-        <Suggestion handleClick={this.props.handleClick} active={active}>
+        <Suggestion key={d} handleClick={this.handleClick} active={active}>
           {d}
         </Suggestion>
       );
     });
 
-    return <div className="list-group">{suggestions}</div>;
+    return (
+      <React.Fragment>
+        <div className="input-group">
+          <input
+            className="form-control bg-light border-0"
+            type="text"
+            placeholder="Search by tag"
+            value={this.props.value}
+            onChange={this.handleChange}
+            onBlur={() => {
+              this.setVisibility(false);
+            }}
+            onKeyDown={this.handleKeyDown}
+          />
+          <div className="input-group-append">
+            <button className="btn bg-light">
+              <i className="fa fa-search text-muted" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+        {this.state.visible ? (
+          <div className="list-group">{suggestions}</div>
+        ) : null}
+      </React.Fragment>
+    );
   }
 }
 
@@ -41,11 +118,11 @@ const Suggestion = props => {
 
   return (
     <button
-      key={children}
       className={
         "list-group-item list-group-item-action py-2" +
         (active ? " active" : "")
       }
+      value={children}
       onClick={props.handleClick}
     >
       {children}
--- src/client/src/components/SearchBar.js
+++ src/client/src/components/SearchBar.js
@@ -4,67 +4,19 @@ import AutoSuggest from "./AutoSuggest";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { tag: "", visible: false, index: undefined };
+    this.state = { value: "" };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.setVisibility = this.setVisibility.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   handleChange(e) {
-    this.setState({
-      tag: e.target.value,
-      index: undefined
-    });
-    this.setVisibility(true);
+    this.setState({ value: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.history.push("/tag/" + this.state.tag);
-  }
-
-  setVisibility(v) {
-    this.setState({ visible: v });
-    if (!v) {
-      this.setState({ index: undefined });
-    }
-  }
-
-  handleKeyDown(e) {
-    if (e.key === "Escape") {
-      this.setVisibility(false);
-    } else if (e.keyCode === 38) {
-      // Arrow up
-      e.preventDefault();
-
-      if (!this.state.visible) return;
-
-      if (this.state.index !== undefined) {
-        this.setState({ index: this.state.index - 1 });
-      } else {
-        this.setState({ index: 0 });
-      }
-    } else if (e.keyCode === 40) {
-      // Arrow down
-      e.preventDefault();
-
-      if (!this.state.visible) return;
-
-      if (this.state.index !== undefined) {
-        this.setState({ index: this.state.index + 1 });
-      } else {
-        this.setState({ index: 0 });
-      }
-    }
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    this.setState({ tag: e.target.textContent });
-    this.setVisibility(false);
+    this.props.history.push("/tag/" + this.state.value);
   }
 
   render() {
@@ -72,32 +24,11 @@ class SearchBar extends Component {
       <div className="row justify-content-center">
         <div className="col-12 col-sm-10 mb-3">
           <form onSubmit={this.handleSubmit} autoComplete="off">
-            <div className="input-group">
-              <input
-                className="form-control bg-light border-0"
-                type="text"
-                placeholder="Search by tag"
-                value={this.state.tag}
-                onChange={this.handleChange}
-                onFocus={() => {
-                  this.setVisibility(true);
-                }}
-                onKeyDown={this.handleKeyDown}
-              />
-              <div className="input-group-append">
-                <button className="btn bg-light">
-                  <i className="fa fa-search text-muted" aria-hidden="true" />
-                </button>
-              </div>
-            </div>
-            {this.state.visible ? (
-              <AutoSuggest
-                data={["caustic arrow", "cleave", "cyclone"]}
-                tag={this.state.tag}
-                index={this.state.index}
-                handleClick={this.handleClick}
-              />
-            ) : null}
+            <AutoSuggest
+              data={["caustic arrow", "cleave", "cyclone"]}
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
           </form>
         </div>
       </div>
