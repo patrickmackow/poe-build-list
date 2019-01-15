43dba6120f460636dd202c87116f95d34164516f
Patrick Mackow <patrickmackow@gmail.com>
2019-01-15 11:57:11 -0500

    Added footer to page.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import styled from "styled-components";
 
 // import "./App.css";
 
@@ -12,13 +13,41 @@ class App extends Component {
     return (
       <BrowserRouter>
         <React.Fragment>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/class/:gameClass" component={ClassBuilds} />
-          <Route exact path="/tag/:tag" component={TagBuilds} />
+          <Wrapper>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/class/:gameClass" component={ClassBuilds} />
+            <Route exact path="/tag/:tag" component={TagBuilds} />
+          </Wrapper>
+          <Footer>
+            <p>This is a fan site not associated with Grinding Gear Games.</p>
+            <a href="">View source</a>
+          </Footer>
         </React.Fragment>
       </BrowserRouter>
     );
   }
 }
+
+const Wrapper = styled.div`
+  overflow: auto;
+  margin-bottom: 1em;
+  flex: 1 0 auto;
+`;
+
+const Footer = styled.footer`
+  flex-shrink: 1;
+  padding: 1em 0;
+  font-size: 0.8rem;
+  background-color: #ddd;
+  text-align: center;
+
+  > * {
+    margin: 0;
+  }
+
+  p {
+    color: rgba(0, 0, 0, 0.4);
+  }
+`;
 
 export default App;
